a44a900a365e458d4cd2432258e58560aea51e4b
dishanayak383 <dishanayak383@gmail.com>
2025-03-03 11:34:18 +0530

    first commit

---
+++ icons/src/Components/Cart.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import './Cart.css';
+
+
+export const Cart =()=> {
+    return (
+        <div className="container"></div>
+    )
+}
+   
+
+
+
+
+
+
+
+
+export default Cart;
---
+++ icons/src/Components/Cart.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import  Cart from  './Cart.css';
+
+export default {
+    title:"Components/Cart",
+    compmonent:Cart,
+};
+
+export const Default njjjjgyuytttggg
