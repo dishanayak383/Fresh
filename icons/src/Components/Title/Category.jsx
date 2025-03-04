c7dac04182f465bc5d624127b8ad485ce049eb17
dishanayak383 <dishanayak383@gmail.com>
2025-03-04 10:58:55 +0530

    first commit

---
+++ icons/src/Components/Title/Category.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Category.css"
+
+const Category =()=> {
+    return (
+        <div className="sub-container">
+            <div className="a1"></div>
+            <div className="a2"></div>
+        </div>
+
+    )
+}
+
+export default Category;
---
+++ icons/src/Components/Title/Category.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import  Category from  './Category';
+
+export default {
+    title:"Components/Category",
+    compmonent:Category,
+};
+const Template=(args)=><Category {...args}/>
+export const category =Template.bind();;
