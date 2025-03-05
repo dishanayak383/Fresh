e1844c253675d8e00d722300efef0b52ee7621ac
dishanayak383 <dishanayak383@gmail.com>
2025-03-05 11:02:01 +0530

    first commit

--- icons/src/Components/Title/Category.jsx
+++ icons/src/Components/Title/Category.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Category.css"
 import { FaCalendarAlt } from "react-icons/fa";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { FaUsers } from "react-icons/fa6";
 
 const Category =()=> {
     return (
@@ -12,10 +14,33 @@ const Category =()=> {
                     </div>
                     <div className="b2">Hello,</div>
                     <div className="b3">Joko Husien</div>
-                    <div className="icon1"><FaCalendarAlt /></div>
-                    <div className="icon2"></div>
+                    <div className="b4"><FaCalendarAlt /></div>
+                    <div className="b5"><IoMdNotificationsOutline /></div>
+                    <div className="b6">On Progress(12)</div>
+                    <div className="b7">View More</div>
+                    <div className="b11">
+                        <div className="b8">
+                            <div className="b10">
+                                <div className="c1">Design UI ToDo App</div>
+                                <div className="c2">Friday, 08 July 2022</div>
+                                <div className="c3"></div>
+                            </div>
+                        </div>
+                        <div className="b9"></div>
+                         
+                    
+
+                    </div>
+                    
+
+                    
                 </div>
+                
+            
+                    
             </div>
+            <div className="b11"></div>
+ 
             <div className="a2"></div>
         </div>
 
